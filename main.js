6e7edbbefa7037fec4616d7013e6f9bdc60f3f5f
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-11-02 10:09:10 +0100

    First steps

---
+++ main.js
@@ -0,0 +1,18 @@
+const filmer = [
+  {
+    navn: "Django Unchained",
+    bildeID: "place holder",
+    sett: true,
+    favoritt: true,
+    beskriveslse: "Dette er en flott film",
+    score: 10,
+  },
+  {
+    navn: "Pulp Fiction",
+    bildeID: "place holder",
+    sett: true,
+    favoritt: true,
+    beskriveslse: "Dette er også en flott film",
+    score: 10,
+  },
+];
