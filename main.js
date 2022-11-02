92a09ccef5a9998652b0f880609b142c59ad4ee4
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2022-11-02 13:04:27 +0100

    Framgang

--- main.js
+++ main.js
@@ -1,18 +1,76 @@
-const filmer = [
+//modell
+const movies = [
   {
-    navn: "Django Unchained",
-    bildeID: "place holder",
-    sett: true,
-    favoritt: true,
-    beskriveslse: "Dette er en flott film",
-    score: 10,
+    name: "Inglourious Basterds",
+    picture: "img/inglourious basterds.png",
+    watched: true,
+    favorite: true,
+    description: "Dette er en flott film",
+    rating: 10,
+    genre: ["Action", "Krig"],
   },
   {
-    navn: "Pulp Fiction",
-    bildeID: "place holder",
-    sett: true,
-    favoritt: true,
-    beskriveslse: "Dette er også en flott film",
-    score: 10,
+    name: "Pulp Fiction",
+    picture: "place holder",
+    watched: true,
+    favorite: true,
+    description: "Dette er også en flott film",
+    rating: 10,
+    genre: [],
   },
 ];
+
+const users = [
+  {
+    username: "Johnny",
+    pasword: "Hemmelig",
+  },
+];
+
+const genreSelection = [];
+//view
+
+const mainViewRender = () => {
+  let mainView = `<div class="main">`;
+  for (let i = 0; i < moviesSelected; i++) {
+    mainView += `<div onclick("spesificViewRender(${i})")>`;
+    mainView += `<img src="${movies[moviesSelected[i]].picture}" alt="${
+      movies[moviesSelected[i]].name
+    }" />`;
+    mainView += `<div>${movies[moviesSelected[i]].rating}</div>`;
+    mainView += "</div>";
+  }
+  mainView += `</div>`;
+  document.getElementById("id-for-hoved-div").innerHTML = mainView;
+};
+
+const spesificViewRender = (userSelect) => {
+  document.getElementById("id-for-hoved-div").innerHTML =
+    moviesSelected[userSelect];
+};
+
+//controller
+
+let moviesSelected = [];
+
+const sorteringClear = () => {
+  moviesSelected = [];
+};
+
+const sorteringSjanger = () => {
+  sorteringClear();
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].genre.includes(sjangerSelected)) {
+      moviesSelected.push(i);
+    }
+  }
+};
+
+const sorteringFavorit = () => {
+  sorteringClear();
+  for (let i = 0; i < movies.length; i++) {
+    if (movies[i].favorite) {
+      moviesSelected.push(i);
+    }
+  }
+};
